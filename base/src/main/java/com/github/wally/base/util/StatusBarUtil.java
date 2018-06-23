1bbf2d62236da9e191ead2cd82f0e802c16a68a5
wally <he100zihao100@126.com>
2018-06-23 20:57:30 +0800

    修复StatusBar调用时Activity已经销毁了造成空指针的问题。

--- base/src/main/java/com/github/wally/base/util/StatusBarUtil.java
+++ base/src/main/java/com/github/wally/base/util/StatusBarUtil.java
@@ -12,14 +12,18 @@ public class StatusBarUtil {
     }
 
     public static void hideStatusBar(Activity activity) {
-        WindowManager.LayoutParams attrs = activity.getWindow().getAttributes();
-        attrs.flags |= WindowManager.LayoutParams.FLAG_FULLSCREEN;
-        activity.getWindow().setAttributes(attrs);
+        if (activity != null && !activity.isFinishing()) {
+            WindowManager.LayoutParams attrs = activity.getWindow().getAttributes();
+            attrs.flags |= WindowManager.LayoutParams.FLAG_FULLSCREEN;
+            activity.getWindow().setAttributes(attrs);
+        }
     }
 
     public static void showStatusBar(Activity activity) {
-        WindowManager.LayoutParams attrs = activity.getWindow().getAttributes();
-        attrs.flags &= ~WindowManager.LayoutParams.FLAG_FULLSCREEN;
-        activity.getWindow().setAttributes(attrs);
+        if (activity != null && !activity.isFinishing()) {
+            WindowManager.LayoutParams attrs = activity.getWindow().getAttributes();
+            attrs.flags &= ~WindowManager.LayoutParams.FLAG_FULLSCREEN;
+            activity.getWindow().setAttributes(attrs);
+        }
     }
 }
